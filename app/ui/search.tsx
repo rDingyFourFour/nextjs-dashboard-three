4f569a223595d9e0947c4c94cf6617fb2a99c1be
rdingyfourfour <roey.shmool@gmail.com>
2025-04-07 18:14:49 -0500

    help

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,7 +5,7 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 
-export default function Search({ placeholder = 'Search...' }) {
+export default function Search() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -22,7 +22,7 @@ export default function Search({ placeholder = 'Search...' }) {
     }
     replace(`${pathname}?${params.toString()}`);
     }, 300);
-  }
+  
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -40,6 +40,5 @@ export default function Search({ placeholder = 'Search...' }) {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-  
-  
+  }
 }
