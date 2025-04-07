70aff60cd1cd15ef8292a2e3bf3de9b8ff8408e1
rdingyfourfour <roey.shmool@gmail.com>
2025-04-07 17:49:41 -0500

    placeholder

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,7 +5,7 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 
-export default function Search() {
+export default function Search({ placeholder = 'Search...' }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -14,7 +14,6 @@ export default function Search() {
     const handleSearch = useDebouncedCallback((term) => {
       console.log(`Searching... ${term}`);
 
-
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -22,10 +21,8 @@ export default function Search() {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
-}
-
-
+    }, 300);
+  }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -34,7 +31,7 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        // placeholder={placeholder}
+        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
@@ -43,4 +40,6 @@ export default function Search() {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
+  
+  
 }
